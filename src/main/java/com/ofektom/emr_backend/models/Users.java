2508e7b64ecc358bd17319c7ffd30480aeddf3a6
ofektom <okpohot@gmail.com>
2025-05-06 22:40:21 +0100

    second commit

--- src/main/java/com/ofektom/emr_backend/models/Users.java
+++ src/main/java/com/ofektom/emr_backend/models/Users.java
@@ -1,4 +1,13 @@
 package com.ofektom.emr_backend.models;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
 public class Users {
+    Long id;
+    String name;
+    String designation;
+    Departments department;
 }
